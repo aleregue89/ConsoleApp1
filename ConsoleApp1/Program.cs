c6db4f61b538be13cd58b71969e39f200f1a99bd
aleregue89 <41762402+aleregue89@users.noreply.github.com>
2019-01-10 22:13:01 -0500

    Add files via upload

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,11 +18,11 @@ namespace ConsoleApp1
             string answer5 = "";
             int counter = 0;
             int score = 0;
-            int question1_score = 0;
-            int question2_score = 0;
-            int question3_score = 0;
-            int question4_score = 0;
-            int question5_score = 0;
+            //int question1_score = 0;
+            //int question2_score = 0;
+            //int question3_score = 0;
+            //int question4_score = 0;
+            //int question5_score = 0;
 
             Console.WriteLine("Write carefully the next 7 questions and pick the correct answer for each case");
             Console.WriteLine("Who won the 1991 Italy World Cup?");
@@ -41,7 +41,8 @@ namespace ConsoleApp1
             if (answer1 == "a")
             {
                 Console.WriteLine("correct");
-                question1_score= counter + 10;
+                //question1_score= counter + 10;
+                score += 10;
             } else
             {
                 Console.WriteLine("incorrect");
@@ -62,7 +63,8 @@ namespace ConsoleApp1
             if (answer2 == "h")
             {
                 Console.WriteLine("correct");
-                question2_score = counter + 10;
+                //question2_score = counter + 10;
+                score+=10;
             }
             else
             {
@@ -84,7 +86,8 @@ namespace ConsoleApp1
             if (answer3 == "k")
             {
                 Console.WriteLine("correct");
-                question3_score=counter + 10;
+                //question3_score=counter + 10;
+                score += 10;
             }
             else
             {
@@ -106,7 +109,8 @@ namespace ConsoleApp1
             if (answer4 == "p")
             {
                 Console.WriteLine("correct");
-                question4_score = counter + 10;
+                //question4_score = counter + 10;
+                score += 10;
             }
             else
             {
@@ -128,14 +132,15 @@ namespace ConsoleApp1
             if (answer5 == "q")
             {
                 Console.WriteLine("correct");
-                question5_score = counter + 10;
+                //question5_score = counter + 10;
+                score += 10;
             }
             else
             {
                 Console.WriteLine("incorrect");
             }
 
-            score = question1_score + question2_score + question3_score + question4_score + question5_score;
+           // score = question1_score + question2_score + question3_score + question4_score + question5_score;
 
             Console.WriteLine(score);
 
