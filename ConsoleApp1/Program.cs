a9ff24218cea94840d64a63844b00f1589135033
aleregue89 <41762402+aleregue89@users.noreply.github.com>
2019-01-10 20:48:26 -0500

    Add files via upload

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,12 +16,17 @@ namespace ConsoleApp1
             string answer3 = "";
             string answer4 = "";
             string answer5 = "";
+            int counter = 0;
             int score = 0;
-            
+            int question1_score = 0;
+            int question2_score = 0;
+            int question3_score = 0;
+            int question4_score = 0;
+            int question5_score = 0;
 
             Console.WriteLine("Write carefully the next 7 questions and pick the correct answer for each case");
             Console.WriteLine("Who won the 1991 Italy World Cup?");
-            Console.WriteLine("A: Germany.");
+            Console.WriteLine("A: Germany");
             Console.WriteLine("B: Italy");
             Console.WriteLine("C: Argentina");
             Console.WriteLine("D: Brazil");
@@ -36,7 +41,7 @@ namespace ConsoleApp1
             if (answer1 == "a")
             {
                 Console.WriteLine("correct");
-                score++;
+                question1_score= counter + 10;
             } else
             {
                 Console.WriteLine("incorrect");
@@ -51,12 +56,13 @@ namespace ConsoleApp1
             //getting the input form the console
             Console.WriteLine("Enter your answer: ");
             answer2 = Console.ReadLine();
+            answer2 = answer2.ToLower();
 
             //checking if the answer is correct or not
             if (answer2 == "h")
             {
                 Console.WriteLine("correct");
-                score++;
+                question2_score = counter + 10;
             }
             else
             {
@@ -72,12 +78,13 @@ namespace ConsoleApp1
             //getting the input form the console
             Console.WriteLine("Enter your answer: ");
             answer3 = Console.ReadLine();
+            answer3 = answer3.ToLower();
 
             //checking if the answer is correct or not
             if (answer3 == "k")
             {
                 Console.WriteLine("correct");
-                score++;
+                question3_score=counter + 10;
             }
             else
             {
@@ -93,12 +100,13 @@ namespace ConsoleApp1
             //getting the input form the console
             Console.WriteLine("Enter your answer: ");
             answer4 = Console.ReadLine();
+            answer4 = answer4.ToLower();
 
             //checking if the answer is correct or not
             if (answer4 == "p")
             {
                 Console.WriteLine("correct");
-                score++;
+                question4_score = counter + 10;
             }
             else
             {
@@ -114,17 +122,20 @@ namespace ConsoleApp1
             //getting the input form the console
             Console.WriteLine("Enter your answer: ");
             answer5 = Console.ReadLine();
+            answer5 = answer5.ToLower();
 
             //checking if the answer is correct or not
             if (answer5 == "q")
             {
                 Console.WriteLine("correct");
-                score++;
+                question5_score = counter + 10;
             }
             else
             {
                 Console.WriteLine("incorrect");
             }
+
+            score = question1_score + question2_score + question3_score + question4_score + question5_score;
 
             Console.WriteLine(score);
 
